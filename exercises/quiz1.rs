4807bc369e8d2d721aed08b3081cd2e358cc0992
Chirag Saraswat <csaraswat87@gmail.com>
2023-08-26 17:27:47 +0530

    quiz_1 completed

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,12 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity:u32) -> u32 {
+    let price_of_apple = if quantity > 40 { 1 } else { 2 };
+    price_of_apple * quantity 
+
+}
 
 // Don't modify this function!
 #[test]
